1d849b55577cd731325e565259d4f19a5a6aed4a
akinnimi emmanuel <akinnimiemmanuel@gmail.com>
2023-01-24 10:06:33 +0100

    new files

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
-#this script configures nginx in a new server
-sudo apt-get -y update
-sudo apt-get -y install nginx
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
+# Bash script that sets up your web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo mkdir /data/
+sudo mkdir /data/web_static/
+sudo mkdir /data/web_static/releases/
+sudo mkdir /data/web_static/shared/
+sudo mkdir /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; autoindex off;}' /etc/nginx/sites-available/default
+echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
+sudo chown -R ubuntu:ubuntu /data
+sudo sed -i '37i\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
 sudo service nginx restart
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,48 +1,40 @@
 #!/usr/bin/python3
-"""
-Fab module to create a targ file of all my static files
-"""
-from fabric.api import local, run, env, put, sudo
-from datetime import datetime
-from os import path
-env.hosts = ['35.196.49.136', '107.23.126.192']
+from fabric.api import run, local, put, env
+import datetime
+import os
 
 
 def do_pack():
-    """
-    function to pack in .tgz
-    """
-    local("mkdir -p versions")
-    now = datetime.today()
+    """do pack"""
+    time = datetime.datetime.now()
+    date = (str(time.year) + str(time.month) + str(time.day) + str(time.hour) +
+            str(time.minute) + str(time.second))
     try:
-        file_name = "web_static_{}{}{}{}{}{}.tgz".format(now.year, now.month,
-                                                         now.day, now.hour,
-                                                         now.minute,
-                                                         now.second)
-        local("tar -cvzf versions/{} web_static".format(file_name))
-        return (file_name)
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz ./web_static".format(date))
+        return "./versions/web_static_{}.tgz".format(date)
     except:
-        return (None)
+        return None
+
+
+env.hosts = ['35.229.42.147', '54.90.157.131']
 
 
 def do_deploy(archive_path):
-    """
-    logic to deploy into ssh servers
-    """
-    if not path.isfile(archive_path):
+    if os.path.exists(archive_path) is False:
         return False
     try:
-        put(archive_path, "/tmp/")
-        directory_path = archive_path.split(".")[0]
-        directory_path = directory_path.split("/")[-1]
-        archive_path = archive_path.split("/")[-1]
-        sudo("mkdir -p /data/web_static/releases/{}/".format(directory_path))
-        full_path = "/data/web_static/releases/{}".format(directory_path)
-        sudo("tar -xvzf /tmp/{} -C {}".format(archive_path, full_path))
-        sudo("rm -rf /tmp/{}".format(archive_path))
-        sudo("mv -f {}/web_static/* {}".format(full_path, full_path))
-        sudo("rm -rf /data/web_static/current")
-        sudo("ln -sf {} /data/web_static/current".format(full_path))
-        return(True)
+        upload = put(archive_path, "/tmp/")
+        name = archive_path[9:-4]
+        rmt_path = "/data/web_static/releases/" + name
+        run("mkdir {}".format(rmt_path))
+        run("tar -xvzf /tmp/{}.tgz --directory {}/".format(name, rmt_path))
+        run("rm /tmp/{}.tgz".format(name))
+        run("rm /data/web_static/current")
+        run("ln -nsf /data/web_static/releases/{} /data/web_static/current"
+            .format(name))
+        run("mv {}/web_static/* {}".format(rmt_path, rmt_path))
+        run("rm -d {}/web_static/".format(rmt_path))
+        return True
     except:
-        return(False)
+        return False
